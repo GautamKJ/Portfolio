5c83bdb861d5a694c1b4b927c0cdafe22991d1cb
Gautam KJ <97722291+GautamKJ@users.noreply.github.com>
2024-02-12 22:45:58 +0530

    Update resume

--- src/data/constants.js
+++ src/data/constants.js
@@ -8,7 +8,7 @@ export const Bio = {
     "I am a motivated and versatile individual, always eager to take on new challenges. With a passion for learning I am dedicated to delivering high-quality results. With a positive attitude and a growth mindset, I am ready to make a meaningful contribution and achieve great things.",
   github: "https://github.com/GautamKJ",
   resume:
-    "https://drive.google.com/file/d/10tSZWnd0fkv41haBJXMni2hm0klEoyPi/view?usp=drive_link",
+    "https://drive.google.com/file/d/1KEULMr0Y9t1nkdx4Ny7hJhJQYnCwJQDM/view?usp=sharing",
   linkedin: "https://www.linkedin.com/in/gautam-jha-44920b224",
   // twitter: "https://twitter.com/RishavChanda",
   insta: "https://www.instagram.com/gautamjha306/?igsh=MWZscnBjY2lpd2N6bQ%3D%3D",
